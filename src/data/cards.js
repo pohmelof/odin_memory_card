4d4e4ac2a2821285979fb0ecb6113e517aff0921
Alex <pohmelof@gmail.com>
2023-06-26 14:16:39 +0300

    fixed image links

--- src/data/cards.js
+++ src/data/cards.js
@@ -2,85 +2,85 @@ import { nanoid } from "nanoid";
 
 const cardsData = [
     {
-      imgSrc: '/assets/images/Audi.png',
+      imgSrc: './assets/images/Audi.png',
       imgAlt: 'Audi logo',
       imgHeading: 'Audi',
       imgId: nanoid()
     },
     {
-      imgSrc: '/assets/images/BMW.png',
+      imgSrc: './assets/images/BMW.png',
       imgAlt: 'BMW logo',
       imgHeading: 'BMW',
       imgId: nanoid()
     },
     {
-      imgSrc: '/assets/images/Chevrolet.png',
+      imgSrc: './assets/images/Chevrolet.png',
       imgAlt: 'Chevrolet logo',
       imgHeading: 'Chevrolet',
       imgId: nanoid()
     },
     {
-      imgSrc: '/assets/images/Ferrari.png',
+      imgSrc: './assets/images/Ferrari.png',
       imgAlt: 'Ferrari logo',
       imgHeading: 'Ferrari',
       imgId: nanoid()
     },
     {
-      imgSrc: '/assets/images/Ford.png',
+      imgSrc: './assets/images/Ford.png',
       imgAlt: 'Ford logo',
       imgHeading: 'Ford',
       imgId: nanoid()
     },
     {
-      imgSrc: '/assets/images/Honda.png',
+      imgSrc: './assets/images/Honda.png',
       imgAlt: 'Honda logo',
       imgHeading: 'Honda',
       imgId: nanoid()
     },
     {
-      imgSrc: '/assets/images/Hyundai.png',
+      imgSrc: './assets/images/Hyundai.png',
       imgAlt: 'Hyundai logo',
       imgHeading: 'Hyundai',
       imgId: nanoid()
     },
     {
-      imgSrc: '/assets/images/Mazda.png',
+      imgSrc: './assets/images/Mazda.png',
       imgAlt: 'Mazda logo',
       imgHeading: 'Mazda',
       imgId: nanoid()
     },
     {
-      imgSrc: '/assets/images/Mercedes.png',
+      imgSrc: './assets/images/Mercedes.png',
       imgAlt: 'Mercedes logo',
       imgHeading: 'Mercedes',
       imgId: nanoid()
     },
     {
-      imgSrc: '/assets/images/Nissan.png',
+      imgSrc: './assets/images/Nissan.png',
       imgAlt: 'Nissan logo',
       imgHeading: 'Nissan',
       imgId: nanoid()
     },
     {
-      imgSrc: '/assets/images/Opel.png',
+      imgSrc: './assets/images/Opel.png',
       imgAlt: 'Opel logo',
       imgHeading: 'Opel',
       imgId: nanoid()
     },
     {
-      imgSrc: '/assets/images/Porsche.png',
+      imgSrc: './assets/images/Porsche.png',
       imgAlt: 'Porsche logo',
       imgHeading: 'Porsche',
       imgId: nanoid()
     },
     {
-      imgSrc: '/assets/images/toyota.svg',
+      imgSrc: './assets/images/toyota.svg',
       imgAlt: 'Toyota logo',
       imgHeading: 'Toyota',
       imgId: nanoid()
     },
     {
-      imgSrc: '/assets/images/Volvo.png',
+      imgSrc: './assets/images/Volvo.png',
       imgAlt: 'Volvo logo',
       imgHeading: 'Volvo',
       imgId: nanoid()
